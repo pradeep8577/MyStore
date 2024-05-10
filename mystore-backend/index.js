226dcd94651eadacf89fa602a879fa6da3c3e724
pradeep8577 <pradeeppawar8577@gmail.com>
2024-05-10 19:09:56 +0530

    update cors error

--- mystore-backend/index.js
+++ mystore-backend/index.js
@@ -6,7 +6,9 @@ const cors = require('cors')
 const cookieParser = require('cookie-parser')
 require('dotenv').config()
 
-app.use(express.json({ limit: '5mb' }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(
   cors({
     origin: "*",
