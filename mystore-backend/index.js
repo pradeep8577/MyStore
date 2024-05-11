207a65dc6282f56f686eaa1983747f548d15816c
pradeep8577 <pradeeppawar8577@gmail.com>
2024-05-11 14:52:55 +0530

    update cors link

--- mystore-backend/index.js
+++ mystore-backend/index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "*",
+    origin: ['https://mystore-dashboard.vercel.app','*','lohttp://localhost:4000'],
     credentials: true,
   })
 );
